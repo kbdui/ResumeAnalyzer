4688c43d5b7e138ee5f140edf3eee277c96e4208
kbdui <2915504390@qq.com>
2026-02-17 14:28:28 +0800

    add backend

---
+++ backend/AnalyzerBack/src/main/java/org/example/analyzerback/AnalyzerBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.analyzerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzerBackApplication.class, args);
+	}
+
+}
---
+++ backend/AnalyzerBack/src/test/java/org/example/analyzerback/AnalyzerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.analyzerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
